f400ca3d868208789ff7b3c5780be9ae1035f0de
hb <hb@aroma-project.org>
2016-11-18 20:19:37 +0100

    Now rprofile() acknowledges --no-init-file

--- R/startup.R
+++ R/startup.R
@@ -74,15 +74,22 @@ renviron <- function(paths = c("~", "."), unload = FALSE, debug = NA) {
 #' @export
 rprofile <- function(paths = c("~", "."), unload = FALSE, debug = NA) {
   debug(debug)
-  
+
+  args <- commandArgs()
+
+  ## Skip?
+  skip <- ("--no-init-file" %in% args)
+
   # (i) Check and fix common errors
   check(paths = paths, fix = TRUE)
   
-  # (ii) Load custom .Rprofile.d/* files
-  startup_apply(".Rprofile.d", FUN = source, paths = paths)
+  if (!skip) {
+    # (ii) Load custom .Rprofile.d/* files
+    startup_apply(".Rprofile.d", FUN = source, paths = paths)
   
-  # (iii) Validate .Rprofile encoding
-  check_rprofile_encoding()
+    # (iii) Validate .Rprofile encoding
+    check_rprofile_encoding()
+  }
   
   res <- api()
   if (unload) unload()
