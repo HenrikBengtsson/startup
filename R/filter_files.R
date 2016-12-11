67149797d47e0c1ea92bdc1736bbb2aee10d614f
hb <hb@aroma-project.org>
2016-12-11 01:12:11 -0800

    Add support for negated file specifications, e.g. os!=windows

--- R/filter_files.R
+++ R/filter_files.R
@@ -1,43 +1,43 @@
 filter_files <- function(files, info = sysinfo()) {
-  op <- "="
+  for (op in c("=", "!=")) {
+    ## Parse <key>=<value> and keep only matching ones
+    for (key in c(names(info), "package")) {
+      ## Identify files specifying this <key>=<value>
+      pattern <- sprintf(".*[^a-z]*%s%s([^=,/]*).*", key, op)
+      idxs <- grep(pattern, files, fixed = FALSE)
+      if (length(idxs) == 0) next
   
-  ## Parse <key>=<value> and keep only matching ones
-  for (key in c(names(info), "package")) {
-    ## Identify files specifying this <key>=<value>
-    pattern <- sprintf(".*[^a-z]*%s%s([^=,/]*).*", key, op)
-    idxs <- grep(pattern, files, fixed = FALSE)
-    if (length(idxs) == 0) next
-
-    ## There could be more than one <key>=<name> specification
-    ## per pathname that use the same <key>, e.g. package=nnn.
-    files_tmp <- strsplit(files[idxs], split = ",", fixed = TRUE)
-    files_tmp <- lapply(files_tmp, FUN = function(f) grep(pattern, f, value = TRUE))
-    files_values <- lapply(files_tmp, FUN = function(f) gsub(pattern, "\\1", f))
-
-    if (key == "package") {
-      files_ok <- lapply(files_values, FUN = function(values) {
-        ## Check which packages are installed and can be loaded
-        keep <- lapply(values, FUN = requireNamespace, quietly = TRUE)
-	keep <- unlist(keep, use.names = FALSE)
-	if (op == "!=") keep <- !keep
-        all(keep)
-      })
-      files_ok <- unlist(files_ok, use.names = FALSE)
-      drop <- idxs[!files_ok]
-    } else {
-      ## sysinfo() keys
-      value <- info[[key]]
-      files_ok <- lapply(files_values, FUN = function(values) {
-        keep <- (values == value)
-	if (op == "!=") keep <- !keep
-        all(keep)
-      })
-      files_ok <- unlist(files_ok, use.names = FALSE)
-      drop <- idxs[!files_ok]
-    }
-    
-    if (length(drop) > 0) files <- files[-drop]
-  } ## for (key ...)
+      ## There could be more than one <key>=<name> specification
+      ## per pathname that use the same <key>, e.g. package=nnn.
+      files_tmp <- strsplit(files[idxs], split = ",", fixed = TRUE)
+      files_tmp <- lapply(files_tmp, FUN = function(f) grep(pattern, f, value = TRUE))
+      files_values <- lapply(files_tmp, FUN = function(f) gsub(pattern, "\\1", f))
+  
+      if (key == "package") {
+        files_ok <- lapply(files_values, FUN = function(values) {
+          ## Check which packages are installed and can be loaded
+          keep <- lapply(values, FUN = requireNamespace, quietly = TRUE)
+          keep <- unlist(keep, use.names = FALSE)
+          if (op == "!=") keep <- !keep
+          all(keep)
+        })
+        files_ok <- unlist(files_ok, use.names = FALSE)
+        drop <- idxs[!files_ok]
+      } else {
+        ## sysinfo() keys
+        value <- info[[key]]
+        files_ok <- lapply(files_values, FUN = function(values) {
+          keep <- (values == value)
+        if (op == "!=") keep <- !keep
+          all(keep)
+        })
+        files_ok <- unlist(files_ok, use.names = FALSE)
+        drop <- idxs[!files_ok]
+      }
+      
+      if (length(drop) > 0) files <- files[-drop]
+    } ## for (key ...)
+  } ## for (op ...)
 
   files
 } ## filter_files()
--- tests/filter_files.R
+++ tests/filter_files.R
@@ -6,17 +6,27 @@ message("*** filter_files() ...")
 
 filesets <- list(
   A = c("a" = TRUE, "b" = TRUE, "package=<non-existing-package>" = FALSE, "package=startup" = TRUE),
-  B = c("a,os=linux" = TRUE, "b,os=windows" = FALSE, "os=linux,os=osx" = FALSE, "package=startup,os=linux" = TRUE)
+  B = c("a,os=linux" = TRUE, "b,os=windows" = FALSE, "os=linux,os=osx" = FALSE, "package=startup,os=linux" = TRUE),
+  C = c("a,os=linux" = TRUE, "b,os!=linux" = FALSE, "c,os!=windows" = TRUE),
+  D = c("package!=startup" = FALSE, "package!=<non-existing-package>" = TRUE),
+  E = c("os=linux,package!=startup" = FALSE, "os=linux,package!=<non-existing-package>" = TRUE),
+  F = c("a" = TRUE, "/home/alice/.Rprofile.d/package=foo" = FALSE),
+  G = c("a" = TRUE, "/home/alice/.Rprofile.d/package!=foo" = TRUE)
 )
 
 for (kk in seq_along(filesets)) {
+  message(sprintf("File set #%d (%s) ...", kk, names(filesets)[kk]))
+  
   files <- filesets[[kk]]
   files0 <- names(files)[files]
   files <- names(files)
   print(files)
 
   filesF <- filter_files(files, info = sysinfo)
+  print(filesF)
   stopifnot(identical(filesF, files0))
+
+  message(sprintf("File set #%d (%s) ... DONE", kk, names(filesets)[kk]))
 }
 
 message("*** filter_files() ... DONE")
