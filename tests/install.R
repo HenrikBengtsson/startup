3b34dc9971a55c3b471c7c3bd0988f4dfb1b486c
hb <hb@aroma-project.org>
2016-12-18 18:00:46 -0800

    TESTS: Testing install() / uninstall() as well

--- tests/install.R
+++ tests/install.R
@@ -1,1 +1,23 @@
+message("*** install() / uninstall() ...")
+
 print(startup:::is_installed())
+
+path <- tempdir()
+
+res <- startup:::is_installed(path = path)
+print(res)
+stopifnot(!isTRUE(res))
+
+print(startup:::install(path = path))
+
+res <- startup:::is_installed(path = path)
+print(res)
+stopifnot(isTRUE(res))
+
+print(startup:::uninstall(path = path))
+
+res <- startup:::is_installed(path = path)
+print(res)
+stopifnot(!isTRUE(res))
+
+message("*** install() / uninstall() ... DONE")
