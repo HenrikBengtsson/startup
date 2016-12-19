0f8175a819c80a715a778b930c2248a2cd6c47a0
hb <hb@aroma-project.org>
2016-12-19 13:34:07 -0800

    CLEANUP: Reordering code to make it easier to split up later

--- R/startup_apply.R
+++ R/startup_apply.R
@@ -4,10 +4,24 @@ startup_apply <- function(what = c("Renviron", "Rprofile"), sibling = FALSE, all
   if (what == "Renviron") {
     paths <- find_renviron_d(sibling = sibling, all = all)
     files <- list_d_files(paths)
-    FUN <- readRenviron
   } else if (what == "Rprofile") {
     paths <- find_rprofile_d(sibling = sibling, all = all)
     files <- list_d_files(paths)
+  }
+
+  ## Nothing to do?
+  if (length(files) == 0) return(invisible(character(0)))
+
+  ## Parse <key>=<value> and keep only matching ones
+  files <- filter_files(files)
+
+  ## Nothing to do?
+  if (length(files) == 0) return(invisible(character(0)))
+
+  ## How should the files be processed?
+  if (what == "Renviron") {
+    FUN <- readRenviron
+  } else if (what == "Rprofile") {
     FUN <- function(pathname) {
       res <- tryCatch(source(pathname, print.eval = print.eval), error = identity)
       if (inherits(res, "error")) {
@@ -26,25 +40,15 @@ startup_apply <- function(what = c("Renviron", "Rprofile"), sibling = FALSE, all
     }
   }
 
-  ## Nothing to do?
-  if (length(files) == 0) return(invisible(character(0)))
-
-  ## Parse <key>=<value> and keep only matching ones
-  nfiles <- length(files)
-  files <- filter_files(files)
-
-  ## Nothing to do?
-  if (length(files) == 0) return(invisible(character(0)))
-  
   dryrun <- as.logical(Sys.getenv("R_STARTUP_DRYRUN", "FALSE"))
   dryrun <- getOption("startup.dryrun", dryrun)
-  nfiles <- length(files)
-  logf("Processing %d %s files ...", nfiles, what)
+
+  logf("Processing %d %s files ...", length(files), what)
   for (file in files) {
     logf(" - %s", file)
     if (!dryrun) FUN(file)
   }
-  logf("Processing %d %s files ... done", nfiles, what)
+  logf("Processing %d %s files ... done", length(files), what)
   if (dryrun) log("(all files were skipped because startup.dryrun = TRUE)")
 
   invisible(files)
