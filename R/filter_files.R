1e900c7517a43e2f15fcf7f1ae365a3946453d41
hb <hb@aroma-project.org>
2017-04-30 13:24:39 -0700

    REFACTORING: specific internal functions filter_files_{info|package}() [#16]

--- R/filter_files.R
+++ R/filter_files.R
@@ -6,10 +6,11 @@ list_of_values <- function(files, pattern) {
   lapply(files, FUN = function(f) gsub(pattern, "\\1", f))
 }
 
-filter_files <- function(files, info = sysinfo()) {
+
+filter_files_info <- function(files, info = sysinfo()) {
   for (op in c("=", "!=")) {
     ## Parse <key>=<value> and keep only matching ones
-    for (key in c(names(info), "package")) {
+    for (key in names(info)) {
       ## Identify files specifying this <key>=<value>
       pattern <- sprintf(".*[^a-z]*%s%s([^=,/]*).*", key, op)
       idxs <- grep(pattern, files, fixed = FALSE)
@@ -19,40 +20,65 @@ filter_files <- function(files, info = sysinfo()) {
       ## per pathname that use the same <key>, e.g. package=nnn.
       files_values <- list_of_values(files[idxs], pattern = pattern)
 
-      if (key == "package") {
-        files_ok <- lapply(files_values, FUN = function(values) {
-          ## Check which packages are installed and can be loaded
-          keep <- lapply(values, FUN = is_package_installed)
-          keep <- unlist(keep, use.names = FALSE)
-          if (op == "!=") keep <- !keep
-          all(keep)
-        })
-        files_ok <- unlist(files_ok, use.names = FALSE)
-        drop <- idxs[!files_ok]
-      } else {
-        ## sysinfo() keys
-        value <- info[[key]]
-        if (is.logical(value)) {
-          files_values <- unlist(files_values, use.names = FALSE)
-          files_values <- toupper(files_values)
-          files_values[files_values == "1"] <- "TRUE"
-          files_values[files_values == "0"] <- "FALSE"
-          files_values <- as.logical(files_values)
-          files_values[is.na(files_values)] <- FALSE
-        }
-        files_ok <- lapply(files_values, FUN = function(values) {
-          keep <- (values == value)
-          if (op == "!=") keep <- !keep
-          all(keep)
-        })
-        files_ok <- unlist(files_ok, use.names = FALSE)
-        drop <- idxs[!files_ok]
+      ## sysinfo() keys
+      value <- info[[key]]
+      if (is.logical(value)) {
+        files_values <- unlist(files_values, use.names = FALSE)
+        files_values <- toupper(files_values)
+        files_values[files_values == "1"] <- "TRUE"
+        files_values[files_values == "0"] <- "FALSE"
+        files_values <- as.logical(files_values)
+        files_values[is.na(files_values)] <- FALSE
       }
+      files_ok <- lapply(files_values, FUN = function(values) {
+        keep <- (values == value)
+        if (op == "!=") keep <- !keep
+        all(keep)
+      })
+      files_ok <- unlist(files_ok, use.names = FALSE)
+      drop <- idxs[!files_ok]
 
       if (length(drop) > 0) files <- files[-drop]
     } ## for (key ...)
   } ## for (op ...)
 
+  files
+} ## filter_files_info()
+
+
+filter_files_package <- function(files) {
+  for (op in c("=", "!=")) {
+    ## Parse <key>=<value> and keep only matching ones
+
+    ## Identify files specifying this <key>=<value>
+    pattern <- sprintf(".*[^a-z]*package%s([^=,/]*).*", op)
+    idxs <- grep(pattern, files, fixed = FALSE)
+    if (length(idxs) == 0) next
+
+    ## There could be more than one <key>=<name> specification
+    ## per pathname that use the same <key>, e.g. package=nnn.
+    files_values <- list_of_values(files[idxs], pattern = pattern)
+
+    files_ok <- lapply(files_values, FUN = function(values) {
+      ## Check which packages are installed and can be loaded
+      keep <- lapply(values, FUN = is_package_installed)
+      keep <- unlist(keep, use.names = FALSE)
+      if (op == "!=") keep <- !keep
+      all(keep)
+    })
+    files_ok <- unlist(files_ok, use.names = FALSE)
+    drop <- idxs[!files_ok]
+
+    if (length(drop) > 0) files <- files[-drop]
+  } ## for (op ...)
+
+  files
+} ## filter_files_package()
+
+
+filter_files <- function(files, info = sysinfo()) {
+  files <- filter_files_info(files, info = info)
+  files <- filter_files_package(files)
   files
 } ## filter_files()
 
