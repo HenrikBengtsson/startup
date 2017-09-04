3e09226a749c239f6839d2a61c069672e6a0e24a
hb <hb@aroma-project.org>
2017-09-04 16:41:28 -0700

    WORKAROUND: vignettes are not availble during 'covr' tests - take 3

--- tests/assert-vignettes.R
+++ tests/assert-vignettes.R
@@ -3,15 +3,23 @@
 ## we try to detect it here.
 
 message("*** Assert that package vignettes exist ...")
+message(paste(loadedNamespaces(), sep = "\n"))
+message(paste(Sys.getenv(), sep = "\n"))
 
 ## WORKAROUND: On AppVeyor CI, vignettes are dropped / not built,
 ## and when running covr, R_CMD is not used.
-if (length(packageDescription("startup")$VignetteBuilder) > 0 &&
-    !"covr" %in% loadedNamespaces()) {
+if (length(packageDescription("startup")$VignetteBuilder) &&
+    Sys.getenv("R_COVR") == "") {
   vigns <- utils::vignette(package = "startup")
   print(vigns)
   str(vigns)
   stopifnot(nrow(vigns$results) > 0)
 }
+
+## Test vignette engine registration
+ovalue <- Sys.getenv("R_CMD")
+Sys.setenv("R_CMD" = "dummy")
+startup:::register_vignette_engine_during_build_only("startup")
+Sys.setenv("R_CMD" = ovalue)
 
 message("*** Assert that package vignettes exist ... DONE")
