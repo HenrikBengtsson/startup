a8cbe85f1ae295cd45a88ec38ce33c694acfd87f
hb <hb@aroma-project.org>
2018-07-26 13:49:42 +0200

    ROBUSTNESS: Now backup() will use a filename with a timestamp of millisecond precision if one within the same seconds already exists, i.e. was just created

--- R/backup.R
+++ R/backup.R
@@ -6,6 +6,11 @@ backup <- function(file, quiet = FALSE) {
   
   timestamp <- format(Sys.time(), "%Y%m%d-%H%M%S")
   backup_file <- sprintf("%s.bak.%s", file, timestamp)
+  ## Was another backup file created just before during the same second?
+  if (file.exists(backup_file)) {
+    timestamp <- format(Sys.time(), "%Y%m%d-%H%M%OS3")
+    backup_file <- sprintf("%s.bak.%s", file, timestamp)
+  }
   stop_if_not(!file.exists(backup_file))
   res <- file.copy(file, backup_file, overwrite = FALSE)
 
