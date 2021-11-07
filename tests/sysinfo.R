f500f4fb4e0cf790561eef230e04e6c2dff6de25
Henrik Bengtsson <hb@aroma-project.org>
2021-11-07 09:13:45 -0800

    TESTS: More base R and session info output

--- tests/sysinfo.R
+++ tests/sysinfo.R
@@ -1,5 +1,21 @@
 message("*** sysinfo() ...")
 
+message("- Base R system information:")
+
+utils::str(list(
+  "capabilities()"     = capabilities(),
+  .Machine             = .Machine,
+  .Platform            = .Platform,
+  R.version            = R.version,
+  "Sys.info()"         = as.list(Sys.info())
+))
+
+message("- Session information:")
+print(utils::sessionInfo())
+
+
+message("- sysinfo():")
 print(startup::sysinfo())
+
 
 message("*** sysinfo() ... DONE")
