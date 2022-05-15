a2b7a30393f1bfae6e3f2dda67e0b7414af2d006
Henrik Bengtsson <hb@aroma-project.org>
2022-05-15 11:08:31 -0700

    tweak debug output order

--- R/startup.R
+++ R/startup.R
@@ -187,8 +187,6 @@ startup <- function(sibling = FALSE, all = FALSE,
     if (is_file(f)) logf("- %s", file_info(f, type = "r"))
 
     logf("- R_PROFILE: %s", file_info(Sys.getenv("R_PROFILE")))
-    logf("- R_PROFILE_USER: %s", file_info(Sys.getenv("R_PROFILE_USER")))
-
     no_site_file <- any(c("--no-site-file", "--vanilla") %in% cmd_args)
     if (!no_site_file) {
       f <- Sys.getenv("R_PROFILE")
@@ -204,6 +202,7 @@ startup <- function(sibling = FALSE, all = FALSE,
       }
     }
 
+    logf("- R_PROFILE_USER: %s", file_info(Sys.getenv("R_PROFILE_USER")))
     no_init_file <- any(c("--no-init-file", "--vanilla") %in% cmd_args)
     if (!no_init_file) {
       f <- Sys.getenv("R_PROFILE_USER")
