0f2a5769bb81f68359442615f0cd4749558a454c
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-04-24 12:44:51 -0700

    is_webr(): webr (>= 0.3.2-9000) sets env var 'WEBR' [#133]

--- R/is_webr.R
+++ R/is_webr.R
@@ -6,7 +6,12 @@
 #' 1. WebR - R in the Browser, <https://docs.r-wasm.org/webr/latest/>
 is_webr <- function() {
   if (!"webr" %in% loadedNamespaces()) return(FALSE)
+
   ## Source: https://github.com/r-wasm/webr/issues/414
+  ## In webr (> 0.3.2) [2024-04-23]
+  if (Sys.getenv("WEBR") == "1") return(TRUE)
+
+  ## Fallback: https://github.com/r-wasm/webr/issues/414
   ns <- getNamespace("webr")
   if (!exists("eval_js", mode = "function", envir = ns)) return(FALSE)
   eval_js <- get("eval_js", mode = "function", envir = ns)
