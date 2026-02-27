35640ee6bd954e797937c902ca8dc73d149e353a
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-02-27 15:49:10 -0800

    Update Wine mailing list URL (that said, they return 403 when outside of the browser, e.g. 'R CMD check' will complain

--- R/is_wine.R
+++ R/is_wine.R
@@ -6,7 +6,7 @@
 #' 1. Wine Developer FAQ, How can I detect Wine?,
 #'    \url{https://gitlab.winehq.org/wine/wine/-/wikis/Developer-FAQ#How_can_I_detect_Wine.3F}
 #' 2. Jeff Zaroyko, Detecting Wine, Wine Devel mailing list, 2008-09-29,
-#'    \url{https://list.winehq.org/mailman3/hyperkitty/list/wine-devel@winehq.org/thread/TVQIRXMLJZYTTCMABIIVG7VIP46TQLLX/}
+#'    \url{https://www.winehq.org/pipermail/wine-devel/2008-September/069389.html}
 is_wine <- function() {
   if (.Platform$OS.type != "windows") return(FALSE)
   any(grepl("^WINE", names(Sys.getenv())))
